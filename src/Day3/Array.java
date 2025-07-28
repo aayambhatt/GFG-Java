726336c149e60098819b9eebc85ebdbbaeec87bf
aayambhatt <aayambhatt23@gmail.com>
2025-07-28 16:13:47 +0530

    Array

--- src/Day3/Array.java
+++ src/Day3/Array.java
@@ -1,4 +1,17 @@
 package Day3;
 
+import java.util.Arrays;
+
 public class Array {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5};
+        String[] nameArr = {"Aayam", "Aaryan", "Anamika", "Sunil"};
+
+        System.out.println(nums[0]);
+        System.out.println(nameArr[2]);
+
+        // to print the array, we need to use Arrays.toString
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString((nameArr)));
+    }
 }
