d1c8b7930dd1b5668d736cc4a9b136214cb7d91c
aayambhatt <aayambhatt23@gmail.com>
2025-07-28 16:12:34 +0530

    variables simple

---
+++ src/Day4/Variables.java
@@ -0,0 +1,14 @@
+package Day4;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 23;
+        String name = "Aayam";
+        double salary = 90000.50;
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+
+    }
+}
