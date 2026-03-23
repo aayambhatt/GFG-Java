f7fb5b57dae13a3e8710c053d366ff2be087962c
aayambhatt <aayambhatt23@gmail.com>
2026-03-23 22:31:08 +0530

    Day-5

---
+++ src/java_reborn/loops/Loops.java
@@ -0,0 +1,15 @@
+package java_reborn.loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        // comma seprated version
+        for (int i = 1, j = 1 ; i <= 10; i++, j+=2){
+            System.out.println(i*j);
+        }
+
+
+
+    }
+
+
+}
