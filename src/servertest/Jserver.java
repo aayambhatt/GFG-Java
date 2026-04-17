e3a4ea7d20fe52e0c559a69214008d54624dd8a5
aayambhatt <aayambhatt23@gmail.com>
2026-04-18 01:08:18 +0530

    http server

--- src/servertest/Jserver.java
+++ src/servertest/Jserver.java
@@ -1,19 +1,35 @@
 package servertest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Jserver {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(8080);
-        Socket client = server.accept();
-        InputStream browserSentMeThis = client.getInputStream();
-        System.out.println(new BufferedReader(new InputStreamReader(browserSentMeThis)).readLine());
-        System.out.println("done");
-        System.out.println(client.getInetAddress()); // equivalent of IPv4’s 127.0.0.1 address.
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(8080);
+
+            while (true) {
+
+                Socket client = server.accept(); // returns a new socket
+                InputStream browserSentMeThis = client.getInputStream();
+                System.out.println(new BufferedReader(new InputStreamReader(browserSentMeThis)).readLine());
+
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                out.writeBytes("HTTP/1.1 200 OK\r\n");
+                out.writeBytes("Content-Type: text/html\r\n\r\n");
+                out.writeBytes("<html><head></head><body><h1>Aayam</h1></body></html>");
+
+                out.flush();
+                out.close();
+
+                System.out.println("done");
+                System.out.println(client.getInetAddress()); // equivalent of IPv4’s 127.0.0.1 address.
+
+            }
+        }
+        catch (IOException e){
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 }
