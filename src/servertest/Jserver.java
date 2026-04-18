b9c9e69789198323f31e297f3c5c65dd7919888a
aayambhatt <aayambhatt23@gmail.com>
2026-04-18 18:27:58 +0530

    http server, helper method,routing

--- src/servertest/Jserver.java
+++ src/servertest/Jserver.java
@@ -13,22 +13,45 @@ public class Jserver {
 
                 Socket client = server.accept(); // returns a new socket
                 InputStream browserSentMeThis = client.getInputStream();
-                System.out.println(new BufferedReader(new InputStreamReader(browserSentMeThis)).readLine());
 
-                DataOutputStream out = new DataOutputStream(client.getOutputStream());
-                out.writeBytes("HTTP/1.1 200 OK\r\n");
-                out.writeBytes("Content-Type: text/html\r\n\r\n");
-                out.writeBytes("<html><head></head><body><h1>Aayam</h1></body></html>");
+                BufferedReader br = new BufferedReader(new InputStreamReader(browserSentMeThis));
+                String firstLine = br.readLine();
+                System.out.println(firstLine);
+                String[] parts = firstLine.split(" ");
+                String path = parts[1];
 
-                out.flush();
-                out.close();
+                if(path.equals("/about")){
+                   sendResponse(client, "200 OK", "<h1>About</h1>");
+                }
 
+                else if(path.equals("/")) {
+                    sendResponse(client, "200 OK", "<h1>Home</h1>");
+                }
+
+                else {
+                    sendResponse(client, "404 Not Found", "<h1>Not Found</h1>");
+                }
                 System.out.println("done");
                 System.out.println(client.getInetAddress()); // equivalent of IPv4’s 127.0.0.1 address.
 
             }
         }
         catch (IOException e){
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    static void sendResponse(Socket client, String status, String body){
+        try{
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            out.writeBytes(  "HTTP/1.1 " +  status + "\r\n");
+            out.writeBytes("Content-Type: text/html\r\n\r\n");
+            out.writeBytes(body);
+
+            out.flush();
+            out.close();
+
+        } catch (IOException e){
             System.out.println("IOException: " + e.getMessage());
         }
     }
